1dabc78992b9d7e26c5b6d04bd6e86713e7f2813
Leon Fan <leonkind@gmail.com>
2022-03-15 03:45:30 -0500

    some more styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,10 +21,14 @@ export default function NavBar(props) {
         return <NavLink to='/login' style={{ float: 'right', marginRight: '50px', fontSize: '15pt', color:'black' }} activeClassName="active" activeStyle={{fontWeight: "bold", color: "black"}} onClick={clearToken}>logged in as {props.state.name}. Logout</NavLink>
     }
 
+    const editProfile = () => {
+        return <NavLink to='/profile' style={{ float: 'right', marginRight: '50px', fontSize: '15pt', color:'black' }} activeClassName="active" activeStyle={{fontWeight: "bold", color: "black"}}>Edit Profile</NavLink>
+    }
+
     return (
         <div className="nav">
             {props.state.token.length > 0 ? Logout() : Login() }
-            <NavLink to='/profile' style={{ float: 'right', marginRight: '50px', fontSize: '15pt', color:'black' }} activeClassName="active" activeStyle={{fontWeight: "bold", color: "black"}}>Edit Profile</NavLink>
+            {props.state.token.length > 0 ? editProfile() : null }
         </div>
     )
 }
